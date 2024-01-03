7651414bf67d8ee58bddd186896e06140f0a7e05
Basanagouda Patil <basanagouda@theretailinsights.com>
2024-01-03 09:17:40 +0530

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<h1>Welcome to my portpolio</h1>
+<p>Visit</p>
+
+<h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  
+  <style lang="postcss">
+    :global(html) {
+      background-color: theme(colors.gray.100);
+    }
+  </style>
